1a35246628d7d170ceff80a23667846858186728
wanjiasu <11630918+wanjiasu@user.noreply.gitee.com>
2025-12-07 17:29:48 +0800

    add: pm2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "betaione_backend",
       script: "env/bin/python",
-      args: "-m uvicorn main:app --host 0.0.0.0 --port 8000"
+      args: "-m uvicorn app.main:app --host 0.0.0.0 --port 8000"
     }
   ]
 };
